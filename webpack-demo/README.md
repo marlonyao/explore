401cac44d3b9afe42eba9f1cb29df7a70f8e8b24
yaolei <yaolei@meituan.com>
2021-01-30 23:11:55 +0800

    feat(webpack-demo): add webpack-demo, basic setup
    内容：
    按照https://webpack.js.org/guides/getting-started/这里的步骤，完成Basic
    Setup。
    
    运行：
    直接在浏览器中打开index.html

---
+++ webpack-demo/README.md
@@ -0,0 +1,4 @@
+## build
+
+## 内容
+1. 按照webpack快速指南：https://webpack.js.org/guides/getting-started/
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,11 @@
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  // 参考：https://www.lodashjs.com/docs/lodash.join
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
