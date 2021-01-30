89356d9598082a4b5dbe135461914f8f3c46356f
yaolei <yaolei@meituan.com>
2021-01-31 00:11:36 +0800

    feat(webpack-demo): 通过javascript添加图标
    修改内容:
    1. 按照https://webpack.js.org/guides/asset-management/#loading-images
    配置webpack处理图片文件.
    2. 在index.js动态创建一个image对象
    
    打开index.html应该看到Hello webpack后面跟一个版权图标.

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash'
 import './style.css'
+// webpack 会将 icon.png 输出到output directory，并将Icon替换为文件路径
+import Icon from './icon.png'
 
 function component() {
   const element = document.createElement('div');
@@ -8,6 +10,12 @@ function component() {
   // 参考：https://www.lodashjs.com/docs/lodash.join
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello')
+
+  // add the image to our existing div
+  const myIcon = new Image()
+  myIcon.src = Icon
+
+  element.appendChild(myIcon)
 
   return element;
 }
