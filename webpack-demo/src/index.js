703381db5f6ea3a0daae96587981ab618943643a
yaolei <yaolei@meituan.com>
2021-01-30 23:31:11 +0800

    feat(webpack-demo): https://webpack.js.org/guides/getting-started/ 文档第二步Creating a Bundle
    运行：
    0. npm install
    1. npx webpack
    2. 打开index.html

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash'
+
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
   // 参考：https://www.lodashjs.com/docs/lodash.join
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
