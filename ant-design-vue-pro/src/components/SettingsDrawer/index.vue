3e62c17ec064880a546456c8a9b316614a21ee83
yaolei <yaolei@meituan.com>
2021-02-08 23:21:22 +0800

    style(ant-design-vue-pro): 运行 npm run lint --fix

--- ant-design-vue-pro/src/components/SettingsDrawer/index.vue
+++ ant-design-vue-pro/src/components/SettingsDrawer/index.vue
@@ -10,9 +10,9 @@
       width="300px"
     >
       <template v-slot:handle>
-          <div class="handle" @click="visible = !visible">
-              <a-icon type="setting"></a-icon>
-          </div>
+        <div class="handle" @click="visible = !visible">
+          <a-icon type="setting"></a-icon>
+        </div>
       </template>
       <p>Some contents...</p>
       <p>Some contents...</p>
@@ -40,16 +40,16 @@ export default {
 
 <style scoped>
 .handle {
-    position: absolute;
-    top: 240px;
-    right: 300px;
-    width: 48px;
-    height: 48px;
-    background: #1890ff;
-    color: #fff;
-    font-size: 20px;
-    text-align: center;
-    line-height: 48px;
-    border-radius: 5px 0 0 5px;
+  position: absolute;
+  top: 240px;
+  right: 300px;
+  width: 48px;
+  height: 48px;
+  background: #1890ff;
+  color: #fff;
+  font-size: 20px;
+  text-align: center;
+  line-height: 48px;
+  border-radius: 5px 0 0 5px;
 }
 </style>
